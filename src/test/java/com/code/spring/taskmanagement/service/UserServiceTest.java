85e7cf8a2a2c58b10fad0a22e0229ca695536c14
Amaninreal <amaninreallife03@gmail.com>
2025-03-17 16:28:10 +0530

    added parameterized test

--- src/test/java/com/code/spring/taskmanagement/service/UserServiceTest.java
+++ src/test/java/com/code/spring/taskmanagement/service/UserServiceTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
